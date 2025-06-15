e6a17538b01d5ff50994a53d404a24c2665fa7a8
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-15 19:13:54 -0300

    Update login.api.cy.js

--- cypress/e2e/api/login.api.cy.js
+++ cypress/e2e/api/login.api.cy.js
@@ -1,1 +1,36 @@
+describe('Login - API', () => {
+  const BASE_URL = 'https://api.realworld.io/api';
 
+  it('Deve retornar status 200 ao logar com sucesso', () => {
+    cy.request({
+      method: 'POST',
+      url: `${BASE_URL}/users/login`,
+      body: {
+        user: {
+          email: 'usuario@teste.com',
+          password: 'senha123',
+        }
+      }
+    }).then((res) => {
+      expect(res.status).to.eq(200);
+      expect(res.body.user).to.have.property('token');
+    });
+  });
+
+  it('Deve retornar 422 ao usar credenciais inválidas', () => {
+    cy.request({
+      method: 'POST',
+      url: `${BASE_URL}/users/login`,
+      failOnStatusCode: false,
+      body: {
+        user: {
+          email: 'email@invalido.com',
+          password: 'errada',
+        }
+      }
+    }).then((res) => {
+      expect(res.status).to.eq(422);
+      expect(res.body.errors['email or password']).to.include('is invalid');
+    });
+  });
+});
