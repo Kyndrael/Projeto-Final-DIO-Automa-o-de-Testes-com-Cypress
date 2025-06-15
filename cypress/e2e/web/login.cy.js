71dc4a394c952a6c785aabb9791224209ce194c1
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-15 19:13:32 -0300

    Update login.cy.js

--- cypress/e2e/web/login.cy.js
+++ cypress/e2e/web/login.cy.js
@@ -1,1 +1,21 @@
+describe('Login - UI', () => {
+  beforeEach(() => {
+    cy.visit('/login');
+  });
 
+  it('Deve logar com sucesso usando a interface', () => {
+    cy.get('input[placeholder="Email"]').type('usuario@teste.com');
+    cy.get('input[placeholder="Password"]').type('senha123');
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('Your Feed').should('be.visible');
+  });
+
+  it('Deve exibir erro com credenciais inválidas', () => {
+    cy.get('input[placeholder="Email"]').type('invalido@teste.com');
+    cy.get('input[placeholder="Password"]').type('senhaerrada');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.error-messages').should('contain', 'email or password is invalid');
+  });
+});
