2cdb4f9afb31bd2553c9b85ce8fb7458ab801970
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-15 19:30:30 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,9 @@
+const { defineConfig } = require('cypress');
 
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://demo.realworld.io/#',
+    fixturesFolder: 'cypress/fixtures',
+    supportFile: 'cypress/support/e2e.js'
+  }
+});
